69de87a97cecaa88a4e72eacd0fca453bd635928
ealvarez94 <ealvarezmol@uoc.edu>
2023-03-23 18:46:29 +0100

    Initial commit

---
+++ src/test/java/fp8/paracasa/ParacasaApplicationTests.java
@@ -0,0 +1,13 @@
+package fp8.paracasa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParacasaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
